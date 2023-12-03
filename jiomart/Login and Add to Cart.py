ffe24df7d8f3dba01eb5e38ba63e3d862b36f767
G Ajeet <gajeet11000@gmail.com>
2023-12-03 13:17:33 +0530

    made some functions private which are used within the class

--- jiomart/Login and Add to Cart.py
+++ jiomart/Login and Add to Cart.py
@@ -27,17 +27,17 @@ class JioMart:
             headers = {}
             headers["Authtoken"] = data_dict["authtoken"]
             headers["Userid"] = data_dict["userid"]
-            self.add_request_headers(headers)
+            self.__add_request_headers(headers)
         except FileNotFoundError:
             print("File load_storage.txt doesn't exist to load headers")
 
-    def get_timestamp(self):
+    def __get_timestamp(self):
         return str(int(time.time()) * 1000)
 
-    def set_cart_id(self, cart_id):
+    def __set_cart_id(self, cart_id):
         self.cart_id = cart_id
 
-    def add_request_headers(self, headers):
+    def __add_request_headers(self, headers):
         self.request_headers.update(headers)
 
     def create_smart_cart(self):
@@ -48,13 +48,13 @@ class JioMart:
         ).json()
 
         if response["status"] == "success":
-            self.set_cart_id(response["result"]["cart_id"])
+            self.__set_cart_id(response["result"]["cart_id"])
             return True
         else:
             return False, response.text
 
     def add_to_cart(self, product_id):
-        timestamp = self.get_timestamp()
+        timestamp = self.__get_timestamp()
         url = f"https://www.jiomart.com/mst/rest/v1/5/cart/add_item?product_code={product_id}&qty=1&seller_id=1&n={timestamp}&cart_id={self.cart_id}"
 
         response = requests.get(
@@ -67,7 +67,7 @@ class JioMart:
             return False, response.text
 
     def get_cart_items(self):
-        timestamp = self.get_timestamp()
+        timestamp = self.__get_timestamp()
         url = f"https://www.jiomart.com/mst/rest/v1/5/cart/get?n={timestamp}&cart_id={self.cart_id}"
 
         response = requests.get(
