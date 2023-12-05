826ecf175aa202173612c96b0ab2e43ebace1cdf
G Ajeet <gajeet11000@gmail.com>
2023-12-06 01:40:02 +0530

    fixed header bugs

--- jiomart/jio_data.py
+++ jiomart/jio_data.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 import requests
 
@@ -10,20 +11,27 @@ class JioData:
             "X-Oms-Application-Id": "5ea6821b3425bb07c82a25c1",
             "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
         }
-        self.cookies = None
+        self.cookies = {}
         self.cart_id = None
         self.smart_cart_id = None
 
         self.__load_cookies()
         self.__load_headers()
+
+        print(self.cookies)
+        print(self.headers)
+
         self.__check_cart_ids()
         self.change_location()
 
-    def __set_header(self, key, value):
-        self.cookies[key] = value
+    def get_timestamp(self):
+        return str(int(time.time()) * 1000)
 
     def __set_cookie(self, key, value):
-        self.request_headers[key] = value
+        self.cookies[key] = value
+
+    def __set_header(self, key, value):
+        self.headers[key] = value
 
     def __check_cart_ids(self):
         if not self.cart_id:
@@ -40,14 +48,14 @@ class JioData:
 
     def __get_location_details(self, pincode):
         location_url = f"https://www.jiomart.com/mst/rest/v1/5/pin/{pincode}"
-        location_res = requests.get(
-            url=location_url, headers=self.request_headers
-        ).json()
+        location_res = requests.get(url=location_url, headers=self.headers).json()
         return location_res["result"]
 
     def __get_cart_id(self):
-        url = "https://www.jiomart.com/mst/rest/v1/5/cart/get"
+        timestamp = self.get_timestamp()
+        url = f"https://www.jiomart.com/mst/rest/v1/5/cart/get?n={timestamp}"
         res = requests.get(url=url, headers=self.headers, cookies=self.cookies).json()
+        print(res)
 
         if res["status"] == "success":
             self.cart_id = res["result"]["cart"]["id"]
@@ -76,7 +84,7 @@ class JioData:
         self.save_cookies_headers()
 
     def __load_cookies(self):
-        if os.path.exists("cookies.json") and os.path.getsize() != 0:
+        if os.path.exists("cookies.json") and os.path.getsize("cookies.json") != 0:
             with open("cookies.json") as json_file:
                 self.cookies.update(json.load(json_file))
         else:
@@ -86,12 +94,12 @@ class JioData:
                 cookies = {}
                 for cookie in data_dict:
                     cookies[cookie["name"]] = cookie["value"]
-                self.cookies = cookies
+                self.cookies.update(cookies)
             except FileNotFoundError:
                 print("File cookies.txt doesn't exist to load cookies")
 
     def __load_headers(self):
-        if os.path.exists("headers.json") and os.path.getsize() != 0:
+        if os.path.exists("headers.json") and os.path.getsize("headers.json") != 0:
             with open("headers.json") as json_file:
                 self.headers.update(json.load(json_file))
         else:
@@ -101,6 +109,7 @@ class JioData:
                 headers = {}
                 headers["Authtoken"] = data_dict["authtoken"]
                 headers["Userid"] = data_dict["userid"]
+                headers["Pin"] = data_dict["nms_mgo_pincode"]
                 self.headers.update(headers)
 
                 cart_info = data_dict.get("cart_info")
