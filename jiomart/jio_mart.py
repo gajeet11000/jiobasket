81e7cf238e845338849d3cab256ccb2dbc583c1f
G Ajeet <gajeet11000@gmail.com>
2023-12-07 16:37:23 +0530

    added function to remove product from cart

--- jiomart/jio_mart.py
+++ jiomart/jio_mart.py
@@ -9,7 +9,7 @@ class JioMart(JioData):
         super().__init__()
 
     def get_cart_items(self, cart_id):
-        timestamp = self.__get_timestamp()
+        timestamp = self.get_timestamp()
         url = f"https://www.jiomart.com/mst/rest/v1/5/cart/get?n={timestamp}&cart_id={cart_id}"
 
         res = requests.get(url=url, cookies=self.cookies, headers=self.request_headers)
@@ -25,15 +25,37 @@ class JioMart(JioData):
         else:
             return False, res
 
-    def add_to_cart(self, cart_type, product_id, qty, seller_id=1):
-        timestamp = self.__get_timestamp()
+    def add_to_cart(self, cart_type, product_id, seller_id=1, qty=1):
+        timestamp = self.get_timestamp()
 
-        url = f"https://www.jiomart.com/mst/rest/v1/5/cart/add_item?product_code={product_id}&qty={qty}&seller_id={seller_id}&n={timestamp}"
+        url = f"https://www.jiomart.com/mst/rest/v1/5/cart/add_item?product_code={product_id}&qty={qty}&seller_id={seller_id}&n={timestamp}&cart_id="
 
         if cart_type == "regular":
-            url += f"&cart_id={self.cart_id}"
+            url += str(self.cart_id)
         else:
-            url += f"&cart_id={self.smart_cart_id}"
+            url += str(self.smart_cart_id)
+
+        res = requests.get(url=url, cookies=self.cookies, headers=self.request_headers)
+
+        try:
+            res = res.json()
+        except json.JSONDecodeError:
+            print(res.text)
+            raise
+
+        if res["status"] == "success":
+            return True
+        else:
+            return False, res
+
+    def remove_from_cart(self, cart_type, product_id, qty=1):
+        timestamp = self.get_timestamp()
+        url = f"https://www.jiomart.com/mst/rest/v1/5/cart/remove_item?product_code={product_id}&qty={qty}&n={timestamp}&cart_id="
+
+        if cart_type == "regular":
+            url += str(self.cart_id)
+        else:
+            url += str(self.smart_cart_id)
 
         res = requests.get(url=url, cookies=self.cookies, headers=self.request_headers)
 
