badef76ded6554d1dc6460c037f4a6e4c5aaa661
G Ajeet <gajeet11000@gmail.com>
2023-12-10 23:08:55 +0530

    added database class and initialize from csv function

--- jiomart/refresh_database.py
+++ jiomart/refresh_database.py
@@ -1,39 +1,53 @@
-import os, time, requests, django
+import os
+import csv
+import time
+import requests
+import django
 
-# Set up Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
-django.setup()
+from jio_data import JioData
 
-from shopping_aio.models import Product
+# from shopping_aio.models import Product
 
-jiomart_product_url = "https://www.jiomart.com/catalog/productdetails/get/"
 
-products = []
+class Database(JioData):
+    def __init__(self) -> None:
+        super().__init__()
 
-with open("jiomart_product_id.txt") as file:
-    for line in file:
-        products.append(line.strip().replace("\n", ""))
-        
-for product_id in products:
-    
-    product_url = jiomart_product_url + product_id
+        # For connecting Django Model
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+        django.setup()
 
-    response = requests.get(
-        url=product_url, headers=request_header_jiomart, cookies=jiomart_cookies
-    ).json()
-    
-    if response["status"] == "success":
-        product_data = response["data"]["gtm_details"]
-        new_product = Product(
-            name=product_data["name"],
-            price=float(product_data["price"]),
-            brand=product_data["brand"],
-        )
-        try:
-            new_product.save()
-            print(new_product.name + " successfully saved")
-        except Exception as e:
-            print(f"Failed to save data:-\n{e}")
-    else:    
-        print(f"Product with the url below is not available at your pincode\n{product_url}")
-    time.sleep(5)
+        self.__product_url = "https://www.jiomart.com/catalog/productdetails/get/"
+        self.__products = {}
+
+    def __initialize_from_csv(self):
+        with open("product_list.csv", "r") as product_list:
+            csv_reader = csv.DictReader(product_list)
+            for product_row in csv_reader:
+                self.__products[product_row["id"]] = product_row["name"]
+
+
+# for product_id in products:
+#     product_url = jiomart_product_url + product_id
+
+#     response = requests.get(
+#         url=product_url, headers=request_header_jiomart, cookies=jiomart_cookies
+#     ).json()
+
+#     if response["status"] == "success":
+#         product_data = response["data"]["gtm_details"]
+#         new_product = Product(
+#             name=product_data["name"],
+#             price=float(product_data["price"]),
+#             brand=product_data["brand"],
+#         )
+#         try:
+#             new_product.save()
+#             print(new_product.name + " successfully saved")
+#         except Exception as e:
+#             print(f"Failed to save data:-\n{e}")
+#     else:
+#         print(
+#             f"Product with the url below is not available at your pincode\n{product_url}"
+#         )
+#     time.sleep(5)
