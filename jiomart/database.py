4f9830ec1a40b7a2eb01b52568432419d01fff9c
G Ajeet <gajeet11000@gmail.com>
2024-01-02 15:16:44 +0530

    added get_product_details functionality

--- jiomart/database.py
+++ jiomart/database.py
@@ -1,12 +1,13 @@
 import os
 import csv
-import time
+import json
 import requests
 import django
 
+
 from jio_data import JioData
 
-# from shopping_aio.models import Product
+from jiomart_app.models import Product
 
 
 class Database(JioData):
@@ -17,37 +18,126 @@ class Database(JioData):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
         django.setup()
 
-        self.__product_url = "https://www.jiomart.com/catalog/productdetails/get/"
+        self.__base_url = "https://www.jiomart.com/catalog/productdetails/get/"
         self.__products = {}
 
-    def __initialize_from_csv(self):
-        with open("product_list.csv", "r") as product_list:
-            csv_reader = csv.DictReader(product_list)
-            for product_row in csv_reader:
-                self.__products[product_row["id"]] = product_row["name"]
+    def get_product_details(self, product_id):
+        product_url = self.__base_url + product_id
 
+        res = requests.get(url=product_url, cookies=self.cookies, headers=self.headers)
 
-# for product_id in products:
-#     product_url = jiomart_product_url + product_id
+        try:
+            res = res.json()
+        except json.JSONDecodeError:
+            print(res.text)
+            raise
 
-#     response = requests.get(
-#         url=product_url, headers=request_header_jiomart, cookies=jiomart_cookies
-#     ).json()
+        if res["status"] == "success":
+            product_detail = {}
+            data = res["data"]
 
-#     if response["status"] == "success":
-#         product_data = response["data"]["gtm_details"]
-#         new_product = Product(
-#             name=product_data["name"],
-#             price=float(product_data["price"]),
-#             brand=product_data["brand"],
-#         )
-#         try:
-#             new_product.save()
-#             print(new_product.name + " successfully saved")
-#         except Exception as e:
-#             print(f"Failed to save data:-\n{e}")
-#     else:
-#         print(
-#             f"Product with the url below is not available at your pincode\n{product_url}"
-#         )
-#     time.sleep(5)
+            # Scraping the product details from the response
+            product_detail["id"] = data["gtm_details"]["id"]
+            product_detail["name"] = data["gtm_details"]["name"]
+            product_detail["price"] = data["gtm_details"]["price"]
+            product_detail["brand"] = data["gtm_details"]["brand"]
+
+            product_detail["max_qty"] = data["max_qty_in_order"]
+
+            # Fetching required payload for getting seller_id and availability
+            l_idx = data["image_url"].rfind("/")
+            s_idx = data["image_url"].rfind("/", 0, l_idx)
+            article_id = data["image_url"][s_idx + 1 : l_idx]
+
+            category = data["gtm_details"]["category"]
+            vertical = category[0 : category.index("/")].upper()
+
+            tenant_ids = data["tenant_ids"]
+
+            mobile_no = self.get_customer_details()["mobile_no"]
+
+            def get_additional_details(article_id, vertical, tenant_ids, mobile_no):
+                json_data = """
+                {
+                "identifier": "",
+                "to_pincode": "",
+                "customer_details": {
+                    "phone_number": "0",
+                    "pincode": "",
+                    "coordinates": {
+                    "lat": 0,
+                    "long": 0
+                    }
+                },
+                "articles": [
+                    {
+                    "article_id": "",
+                    "vertical": "",
+                    "lookup_inventory": true,
+                    "tenant_ids": "",
+                    "merchant_id": null,
+                    "exchange_details": null
+                    }
+                ]
+                }"""
+
+                # Load JSON string into a dictionary
+                data_dict = json.loads(json_data)
+
+                uuid_value = self.get_UUID()
+                location_data = self.get_location_details(self.headers["Pin"])
+
+                # Update values
+                data_dict["identifier"] = uuid_value
+                data_dict["to_pincode"] = data_dict["customer_details"][
+                    "pincode"
+                ] = location_data["pin"]
+                data_dict["customer_details"]["phone_number"] = mobile_no
+                data_dict["customer_details"]["coordinates"]["lat"] = location_data[
+                    "lat"
+                ]
+                data_dict["customer_details"]["coordinates"]["long"] = location_data[
+                    "lon"
+                ]
+                data_dict["articles"][0].update(
+                    {
+                        "article_id": article_id,
+                        "vertical": vertical,
+                        "tenant_ids": tenant_ids,
+                    }
+                )
+
+                url = "https://www.jiomart.com/platform/logistics/api/v1/promise"
+                res = requests.post(url=url, cookies=self.cookies, headers=self.headers)
+
+                try:
+                    res = res.json()
+                except json.JSONDecodeError:
+                    print(res.text)
+                    raise
+
+                errors = res["articles"]["error"]
+
+                if errors["type"] == None:
+                    return True, res["articles"]["seller_data"]["sellerid"]
+                else:
+                    return False, res
+
+            seller_details = get_additional_details(
+                article_id, vertical, tenant_ids, mobile_no
+            )
+
+            if seller_details[0]:
+                product_detail["availability"] = True
+                product_detail["seller_id"] = seller_details[1]
+            else:
+                print(
+                    "Product with id : ",
+                    product_id,
+                    ", not available at your pincode",
+                    self.headers["Pin"],
+                )
+        else:
+            print("Failed to get details for product with id:", product_id)
+
+        return product_detail
